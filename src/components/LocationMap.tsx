5c3b3deb0ef2922208a2d0fa721b2e965949fa37
Vaibhav-Reddy560 <vaibhav.reddy560@gmail.com>
2025-10-22 21:35:53 +0530

    Update LocationMap.tsx

--- src/components/LocationMap.tsx
+++ src/components/LocationMap.tsx
@@ -3,7 +3,15 @@ import React from 'react';
 const LocationMap = () => {
   return (
     <div className="map-container">
-      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15553.888370957551!2d77.56681!3d12.941615!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae158b11e34d2f%3A0x5f4adbdbab8bd80f!2sBMS%20College%20of%20Engineering!5e0!3m2!1sen!2sin!4v1761149062664!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15553.888370957551!2d77.56681!3d12.941615!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae158b11e34d2f%3A0x5f4adbdbab8bd80f!2sBMS%20College%20of%20Engineering!5e0!3m2!1sen!2sin!4v1761149062664!5m2!1sen!2sin" // This is the updated link
+        width="100%"
+        height="100%"
+        style={{ border: 0 }}
+        allowFullScreen={true}
+        loading="lazy"
+        referrerPolicy="no-referrer-when-downgrade"
+      ></iframe>
     </div>
   );
 };
