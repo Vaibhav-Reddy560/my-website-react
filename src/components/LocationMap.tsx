a3aec819def4887ea952514cd51a8f1f3566c1b5
Vaibhav-Reddy560 <vaibhav.reddy560@gmail.com>
2025-10-22 21:34:47 +0530

    Update LocationMap.tsx

--- src/components/LocationMap.tsx
+++ src/components/LocationMap.tsx
@@ -3,15 +3,7 @@ import React from 'react';
 const LocationMap = () => {
   return (
     <div className="map-container">
-      <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.494723948651!2d77.56385207590833!3d12.940026215357987!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae158b11e445c3%3A0x2476e3d850912123!2sB.M.S.%20College%20of%20Engineering!5e0!3m2!1sen!2sin!4v1697995545347!5m2!1sen!2sin"
-        width="100%"
-        height="100%"
-        style={{ border: 0 }}
-        allowFullScreen={true}
-        loading="lazy"
-        referrerPolicy="no-referrer-when-downgrade"
-      ></iframe>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15553.888370957551!2d77.56681!3d12.941615!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bae158b11e34d2f%3A0x5f4adbdbab8bd80f!2sBMS%20College%20of%20Engineering!5e0!3m2!1sen!2sin!4v1761149062664!5m2!1sen!2sin" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
     </div>
   );
 };
